c52fbfc5b7ba7c309f371ce5d7291c6cbc4ef112
Achyudh Ram <achyudhk@gmail.com>
2017-02-04 03:25:24 +0530

    Add GCP Apache Bench Script

--- apache_bench_gcp.py
+++ apache_bench_gcp.py
@@ -10,7 +10,7 @@ cluster_name = "ap-%s-%s-%s" %(machine_types[8], zones[0], str(num_nodes))
 
 node_type = machine_types[2]
 node_zone = zones[0]
-# subprocess.run('gcloud auth application-default login', shell=True)
+subprocess.run('gcloud auth application-default login', shell=True)
 subprocess.run('gcloud container clusters create %s -m %s -z %s --num-nodes=%s' % (cluster_name, machine_types[2], zones[0], str(num_nodes)), shell=True)
 subprocess.run('gcloud container clusters get-credentials ' + cluster_name, shell=True)
 subprocess.run('kubectl run httpd-node --image=asia.gcr.io/my-project-1470428279137/httpd --port=8080', shell=True)
@@ -18,7 +18,7 @@ subprocess.run('kubectl get deployments && kubectl get pods', shell=True)
 subprocess.run('kubectl expose deployment httpd-node --port=80 --target-port=80 --type="LoadBalancer"', shell=True)
 
 print("Waiting for Apache server to start...")
-time.sleep(60)
+# time.sleep(60)
 
 response = subprocess.run('kubectl get services httpd-node', shell=True, stdout=subprocess.PIPE, universal_newlines=True)
 external_ip = None
@@ -31,12 +31,13 @@ if external_ip is None:
     exit()
 
 for num_conn in [100, 1000, 10000, 100000]:
+    print("Benchmarking httpd server at %s with %d connections..." % (external_ip, num_conn))
     response = subprocess.Popen(['ab -n %d -c %d %s/' % (num_conn, concurrency, external_ip)],
                            shell=True, universal_newlines=True,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-
-    with open("data_apache/gcp_%s_%s_n%d_c%d.txt" % (node_type, node_zone, num_nodes, concurrency), 'w') as txt_file:
+    response.wait()
+    with open("data_apache/gcp_%s_%s_n%d_c%d.txt" % (node_type, node_zone, num_conn, concurrency), 'w') as txt_file:
         txt_file.write("".join(response.stdout.readlines()))
 
 
